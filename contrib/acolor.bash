67a584c784b2b47e69f9ce8a21e9bfeba74685f3
xPMo <PMo_@vivaldi.net>
2019-02-26 10:51:07 -0600

    Start of shell integrations

---
+++ contrib/acolor.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# shellcheck disable=2139,2140
+for cmd in du env lsblk lspci nmap; do
+	command -v "$cmd" > /dev/null && alias "$cmd"="acol $cmd"
+done
+alias ls='acol ls --color'
